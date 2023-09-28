2c0aedb6ac2c49a7a8572546bf4d8b6b13147b8c
shirelzz <99799701+shirelzz@users.noreply.github.com>
2023-09-28 13:59:14 +0300

    Initial Commit

---
+++ My Orders/My_OrdersApp.swift
@@ -0,0 +1,17 @@
+//
+//  My_OrdersApp.swift
+//  My Orders
+//
+//  Created by שיראל זכריה on 28/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct My_OrdersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
