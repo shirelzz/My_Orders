563b9c7bd80b067c1789c8dc9009461237f360b7
shirelzz <99799701+shirelzz@users.noreply.github.com>
2023-10-02 02:57:04 +0300

    make the app better looking

--- My Orders/AllOrdersView.swift
+++ My Orders/AllOrdersView.swift
@@ -22,28 +22,6 @@ struct AllOrdersView: View {
             }
         }
     }
-    
-    
-    @State private var selectedSortingOption: SortingOption = .orderDate // Initialize with a default option
-
-    enum SortingOption: String, CaseIterable, Identifiable {
-        case orderDate = "Order Date"
-        case customerName = "Customer Name"
-        case orderTotal = "Order Total"
-
-        var id: String { self.rawValue }
-    }
-
-    var sortedOrders: [DessertOrder] {
-        switch selectedSortingOption {
-        case .orderDate:
-            return orderManager.getOrders().sorted { $0.orderDate > $1.orderDate }
-        case .customerName:
-            return orderManager.getOrders().sorted { $0.customer.name < $1.customer.name }
-        case .orderTotal:
-            return orderManager.getOrders().sorted { $0.totalPrice > $1.totalPrice }
-        }
-    }
 
     
     var body: some View {
@@ -59,21 +37,6 @@ struct AllOrdersView: View {
             .listStyle(InsetGroupedListStyle())
         }
         
-        VStack {
-                    Picker("Sort By", selection: $selectedSortingOption) {
-                        ForEach(SortingOption.allCases) { option in
-                            Text(option.rawValue)
-                        }
-                    }
-                    .pickerStyle(MenuPickerStyle()) // You can choose a different style if preferred
-
-                    List(sortedOrders) { order in
-                        NavigationLink(destination: OrderDetailsView(order: order)) {
-                            OrderRowView(order: order)
-                        }
-                    }
-                    .navigationBarTitle("All Orders")
-                }
     }
 }
 
--- My Orders/ContentView.swift
+++ My Orders/ContentView.swift
@@ -5,59 +5,82 @@
 //  Created by שיראל זכריה on 28/09/2023.
 //
 
-import SwiftUI
+//import SwiftUI
 
+import SwiftUI
 
 struct ContentView: View {
     
-    @State private var orders: [DessertOrder] = []
     @StateObject private var orderManager = OrderManager.shared
+    
+    @State private var showAllOrders = false
+    @State private var isAddOrderViewPresented = false // This controls the navigation
+
     
     init() {
         // Load orders from UserDefaults when ContentView is initialized
         OrderManager.shared.loadOrders()
-        print(OrderManager.shared.getOrders())
-
     }
-
     
     var body: some View {
-        
-        
         NavigationView {
-            
             VStack {
                 
-                List(orders, id: \.orderID) { order in
-                    NavigationLink(destination: OrderDetailsView(order: order)) {
-                        OrderRowView(order: order)
+                Button(action: {
+                                    isAddOrderViewPresented = true // Activate navigation
+                                }) {
+                    Image(systemName: "plus.circle.fill")
+                            .font(.system(size: 36))
+                            .foregroundColor(.blue)
+                            .padding()
+                            .shadow(radius: 4)
                     }
+                    .padding()
+                    .sheet(isPresented: $isAddOrderViewPresented) {
+                                        AddOrderView(orderManager: orderManager)
                 }
-                .listStyle(InsetGroupedListStyle())
-            }
-        
-            
-            .navigationTitle("Order Management")
-            .navigationBarItems(
+                List {
+                    ForEach(upcomingOrders, id: \.orderID) { order in
+                        NavigationLink(destination: OrderDetailsView(order: order)) {
+                            OrderRowView(order: order)
+                        }
+                    }
+                    .listRowBackground(Color.clear)
+                }
+                .listStyle(PlainListStyle())
                 
-                trailing: HStack {
-                    
-                    NavigationLink(destination: AddOrderView(orderManager: OrderManager.shared)) {
-                        Text("New Order")
-                    }
-                    
-                    NavigationLink(destination: UpcomingOrdersView(orderManager: OrderManager.shared)) {
-                        Text("Upcoming Orders")
-                    }
-                    
-                    NavigationLink(destination: AllOrdersView(orderManager: OrderManager.shared)) {
-                        
-                        Text("All Orders")
-                    }
-                    
+
+                
+                NavigationLink(
+                    destination: AllOrdersView(orderManager: orderManager),
+                    isActive: $showAllOrders
+                ) {
+                    EmptyView()
                 }
-            )
+                .hidden() // Hidden NavigationLink to programmatically trigger navigation
+                
+                Button(action: {
+                    showAllOrders.toggle()
+                }) {
+                    Text("All Orders")
+                        .padding()
+                        .background(Color.blue)
+                        .foregroundColor(.white)
+                        .cornerRadius(8)
+                }
+                .padding()
+            }
+            .navigationBarTitle("Upcoming Orders")
         }
+        .accentColor(.blue)
+        .onAppear {
+            // Sort orders by order date in ascending order
+            orderManager.orders.sort { $0.orderDate < $1.orderDate }
+        }
+    }
+    
+    var upcomingOrders: [DessertOrder] {
+        return orderManager.orders.filter { !$0.isCompleted }
     }
 }
 
@@ -66,3 +89,65 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+
+
+
+//struct ContentView: View {
+//
+//    @State private var orders: [DessertOrder] = []
+//    @StateObject private var orderManager = OrderManager.shared
+//
+//    init() {
+//        // Load orders from UserDefaults when ContentView is initialized
+//        OrderManager.shared.loadOrders()
+//        print(OrderManager.shared.getOrders())
+//
+//    }
+//
+//
+//    var body: some View {
+//
+//
+//        NavigationView {
+//
+//            VStack {
+//
+//                List(orders, id: \.orderID) { order in
+//                    NavigationLink(destination: OrderDetailsView(order: order)) {
+//                        OrderRowView(order: order)
+//                    }
+//                }
+//                .listStyle(InsetGroupedListStyle())
+//            }
+//
+//
+//            .navigationTitle("Order Management")
+//            .navigationBarItems(
+//
+//                trailing: HStack {
+//
+//                    NavigationLink(destination: AddOrderView(orderManager: OrderManager.shared)) {
+//                        Text("New Order")
+//                    }
+//
+//                    NavigationLink(destination: UpcomingOrdersView(orderManager: OrderManager.shared)) {
+//                        Text("Upcoming Orders")
+//                    }
+//
+//                    NavigationLink(destination: AllOrdersView(orderManager: OrderManager.shared)) {
+//
+//                        Text("All Orders")
+//                    }
+//
+//                }
+//            )
+//        }
+//    }
+//}
+
+//struct ContentView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        ContentView()
+//    }
+//}
